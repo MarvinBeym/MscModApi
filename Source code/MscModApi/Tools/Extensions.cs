25430cd926b778e7bd823f834f12cd7c9cec8543
Marvin Beym <Marvin.Beym@gmx.de>
2022-12-09 20:58:39 +0100

    Add float .Map extension method

--- Source code/MscModApi/Tools/Extensions.cs
+++ Source code/MscModApi/Tools/Extensions.cs
@@ -19,6 +19,11 @@ namespace MscModApi.Tools
 			return value.ToXY("On", "Off");
 		}
 
+		public static float Map(this float value, float from1, float to1, float from2, float to2)
+		{
+			return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+		}
+
 		public static string ToXY(this bool value, string trueText, string falseText)
 		{
 			return value ? trueText : falseText;
